ae539bed0c75d6f3eb3d42c9a8e6abbdc9560f85
milangupta1 <milangupta1@gmail.com>
2023-09-03 09:38:16 -0400

    added eslint

---
+++ assets/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true,
+        "es2021": false
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 8,
+        "sourceType": "module"
+    }
+}
